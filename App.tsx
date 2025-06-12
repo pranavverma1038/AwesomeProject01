dc37db514220e001bac387cbffaccc5417da2c49
Pranav Verma <101242598+pranavverma1038@users.noreply.github.com>
2025-06-13 00:12:24 +0530

    adding deep link and touchable

--- App.tsx
+++ App.tsx
@@ -2,23 +2,30 @@ import React from "react";
 import {
   View,
   Text,
-  SafeAreaView
+  SafeAreaView,
+  ScrollView
 } from 'react-native'
 import FlatCards from "./components/FlatCards";
 import ElevatedCards from "./components/ElevatedCards";
 import FancyCard from "./components/FancyCard";
+import ActionCard from "./components/ActionCard";
 
 
 
 function App(){
     return (
+    
     <SafeAreaView>
+      <ScrollView>
       <View>
         <FlatCards/>
         <ElevatedCards/>
         <FancyCard/>
+        <ActionCard/>
       </View>
+      </ScrollView>
     </SafeAreaView>
+   
     )
 }
 
---
+++ components/ActionCard.tsx
@@ -0,0 +1,52 @@
+import { StyleSheet, Text, View,Image,TouchableOpacity,Linking } from 'react-native'
+import React from 'react'
+
+
+export default function ActionCard() {
+    function openWebsite(websiteLink:string){
+        Linking.openURL(websiteLink)
+    }
+
+  return (
+    <View>
+      <Text style={styles.headingText}>ActionCard</Text>
+      <View style={[styles.card,styles.elevatedCard]}>
+        <View style={styles.headingContainer}>
+            <Text style={styles.headerText}>
+                Whats new in Javascript ES - 6
+            </Text>
+        </View>
+        <Image
+        style={styles.cardImage}
+        source={{
+            uri:'https://images.unsplash.com/photo-1501594907352-04cda38ebc29?auto=format&fit=crop&w=800&q=80'
+        }}
+        />
+        <View style={styles.bodyContainer}>
+            <Text numberOfLines={3}>
+                Nestled in the heart of Rajasthan, Jaipur is a city that blends royal heritage with vibrant modern life, offering an unforgettable experience through its colorful bazaars, majestic palaces, and timeless traditions.
+            </Text>
+        </View>
+        <View style={styles.footerContainer}>
+            <TouchableOpacity onPress={()=>{openWebsite("https://www.tripadvisor.in/")}}>
+                <Text>Read More</Text>
+            </TouchableOpacity>
+            
+        </View>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    headingText:{},
+    card:{},
+    elevatedCard:{},
+    headingContainer:{},
+    headerText:{},
+    cardImage:{
+        height:100
+    },
+    bodyContainer:{},
+    footerContainer:{},
+})
